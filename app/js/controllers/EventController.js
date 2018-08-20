1be74a170956f90a12b4c6b8352adc5c6d79afea
Bryan Astacio <B.Astacio@motorcredito.com.do>
2018-08-20 17:49:25 -0400

    added the Security, EventData, GravatarUrlBuilder services and EditProfileController also the EditProfile html

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,46 +1,13 @@
 'use strict';
 
 eventsApp.controller('EventController',
-  function EventController($scope) {
-
+  function EventController($scope, eventData, $log) {
     $scope.sortorder = 'name';
-    $scope.event = {
-      name: "Angular Boot Camp",
-      date: 1359781015626,
-      time: '10:30 am',
-      location: {
-        address: "Google Headquaters",
-        city: "Mountain View",
-        province: "CA"
-      },
-      imgUrl: '/img/angularjs-logo.png',
-      sessions: [
-        {
-          name: "Directives Masterclass introductory",
-          creatorName: "Bob Smith",
-          duration: 1,
-          level: "Advanced",
-          abstract: "In this sesion you will learn the ins and outs of directives!",
-          upVoteCount: 0
-        },
-        {
-          name: "Scopes for fun and profit",
-          creatorName: "John Doe",
-          duration: 2,
-          level: "Introductory",
-          abstract: "This session will take a closer look at scopes. Learn what they do, how they do it, and how to get them to do it for you.",
-          upVoteCount: 0
-        },
-        {
-          name: "Well Behaved Controller",
-          creatorName: "John Doe",
-          duration: 4,
-          level: "Intermediate",
-          abstract: "Controllers are the beginning of everything Angular does. Learn how to craft.",
-          upVoteCount: 0
-        }
-      ]
-    };
+    eventData.getEvent()
+      .$promise.then(
+        function(event){$scope.event = event; console.log(event); },
+        function(response) { console.log(response) }
+      );
 
     $scope.upVoteSession = function(session) {
       session.upVoteCount++;
