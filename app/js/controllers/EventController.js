034409a9ebfab80de885c65ba5e9647429df33d6
Bryan Astacio <B.Astacio@motorcredito.com.do>
2018-08-20 15:38:34 -0400

    added the new event

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -3,13 +3,10 @@
 eventsApp.controller('EventController',
   function EventController($scope) {
 
-    $scope.snippet = '<span style="color:red;">Hi there</span>';
-    $scope.boolValue = false;
-    $scope.mystyle = {color:'red'};
-    $scope.buttonDisabled = true;
+    $scope.sortorder = 'name';
     $scope.event = {
       name: "Angular Boot Camp",
-      date: '1/1/2013',
+      date: 1359781015626,
       time: '10:30 am',
       location: {
         address: "Google Headquaters",
@@ -19,9 +16,9 @@ eventsApp.controller('EventController',
       imgUrl: '/img/angularjs-logo.png',
       sessions: [
         {
-          name: "Directives Masterclass",
+          name: "Directives Masterclass introductory",
           creatorName: "Bob Smith",
-          duration: "1 hr",
+          duration: 1,
           level: "Advanced",
           abstract: "In this sesion you will learn the ins and outs of directives!",
           upVoteCount: 0
@@ -29,7 +26,7 @@ eventsApp.controller('EventController',
         {
           name: "Scopes for fun and profit",
           creatorName: "John Doe",
-          duration: "30 mins",
+          duration: 2,
           level: "Introductory",
           abstract: "This session will take a closer look at scopes. Learn what they do, how they do it, and how to get them to do it for you.",
           upVoteCount: 0
@@ -37,7 +34,7 @@ eventsApp.controller('EventController',
         {
           name: "Well Behaved Controller",
           creatorName: "John Doe",
-          duration: "2 hours",
+          duration: 4,
           level: "Intermediate",
           abstract: "Controllers are the beginning of everything Angular does. Learn how to craft.",
           upVoteCount: 0
