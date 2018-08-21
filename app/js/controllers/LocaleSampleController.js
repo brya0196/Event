8fb99c77033e21253d5948d96d1294341c91207e
Bryan Astacio <brya0196@gmail.com>
2018-08-21 10:14:06 -0400

    added the exceptionHandler services making and override of the original to see its functionality'

--- app/js/controllers/LocaleSampleController.js
+++ app/js/controllers/LocaleSampleController.js
@@ -3,4 +3,6 @@
 eventsApp.controller('LocaleSampleController', function LocaleSampleController($scope, $locale) {
   $scope.myDate = Date.now();
   $scope.myFormat = $locale.DATETIME_FORMATS.fullDate;
+
+  // throw { message: "Error message" };
 });
