9cd495ae4337d2b646ccb0626d131ab080816b3d
Bryan Astacio <brya0196@gmail.com>
2018-08-21 10:06:14 -0400

    added some logic to the TimeoutController to run a $timeout example

--- app/js/controllers/TimeoutSampleController.js
+++ app/js/controllers/TimeoutSampleController.js
@@ -2,4 +2,11 @@
 
 eventsApp.controller('TimeoutSampleController', function TimeoutSampleController($scope, $timeout) {
   
+  var promise = $timeout(function() {
+    $scope.name = "John Doe";
+  }, 3000);
+
+  $scope.cancel = function() {
+    $timeout.cancel(promise);
+  }
 });
