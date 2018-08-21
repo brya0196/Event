329ddc3e5cde0cef3cc5f21d335eedea2a9e445e
Bryan Astacio <brya0196@gmail.com>
2018-08-21 11:31:25 -0400

    added examples of Filter and CookieStore services

--- app/js/filters.js
+++ app/js/filters.js
@@ -5,16 +5,12 @@ eventsApp.filter('durations', function() {
     switch (duration) {
       case 1:
         return "Half Hour";
-        break;
       case 2:
         return "1 Hour";
-        break
       case 3:
         return "Half Day";
-        break
       case 4:
         return "Full Day";
-        break
       default:
         break;
     }
